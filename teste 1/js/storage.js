a13073f29e74190b4b7ee7d94825c03fe2dec9d2
Ztrem <ztrcompany1@gmail.com>
2026-02-22 23:29:28 -0300

    Update storage.js

--- teste 1/js/storage.js
+++ teste 1/js/storage.js
@@ -1,2 +1,15 @@
+function getAlbuns(){
+  return JSON.parse(localStorage.getItem("albuns") || "[]");
+}
 
-
+function saveAlbuns(albuns){
+  localStorage.setItem("albuns", JSON.stringify(albuns));
+}
+
+function getFotos(){
+  return JSON.parse(localStorage.getItem("fotos") || "[]");
+}
+
+function saveFotos(fotos){
+  localStorage.setItem("fotos", JSON.stringify(fotos));
+}
