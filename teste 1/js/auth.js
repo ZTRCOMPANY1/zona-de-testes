aba40b320afb779936ac2af94b9b0a7dd827ac91
Ztrem <ztrcompany1@gmail.com>
2026-02-22 23:29:17 -0300

    Update auth.js

--- teste 1/js/auth.js
+++ teste 1/js/auth.js
@@ -1,2 +1,21 @@
+function login(){
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
 
-
+  const users = JSON.parse(localStorage.getItem("users") || "[]");
+
+  const user = users.find(u => u.email === email && u.senha === senha);
+
+  if(user){
+    localStorage.setItem("loggedUser", email);
+    window.location.href = "albuns.html";
+  } else {
+    alert("Usuário não encontrado.");
+  }
+}
+
+function register(nome,email,senha){
+  const users = JSON.parse(localStorage.getItem("users") || "[]");
+  users.push({nome,email,senha});
+  localStorage.setItem("users", JSON.stringify(users));
+}
